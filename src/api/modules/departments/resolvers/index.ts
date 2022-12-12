1ae3a316f0b804f15f1feb72aaff47ed1f74d56f
Samuel Kirigha <dkirigha18@gmail.com>
2022-12-12 17:51:03 +0300

    modified the files

--- src/api/modules/departments/resolvers/index.ts
+++ src/api/modules/departments/resolvers/index.ts
@@ -1,6 +1,6 @@
 import {CreateDepartmentResolver} from "./create-department-resolver";
 import {GetDepartmentsResolver} from "./get-departments-resolver";
-
+ 
 export {
     CreateDepartmentResolver,
     GetDepartmentsResolver
